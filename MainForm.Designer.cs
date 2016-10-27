1bf2d83c07e1a201c9754b178b92b3edf4b8f81e
mlinares <miguel.linares@tecnalia.com>
2016-10-27 10:38:42 +0200

    Delete black frames from video and Extract different frames

---
+++ MainForm.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace BlackFrameEraser
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.btnProcesaVideo = new System.Windows.Forms.Button();
+            this.btnExtraer = new System.Windows.Forms.Button();
+            this.comboBoxVISION = new System.Windows.Forms.ComboBox();
+            this.checkBuenos = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbVideo = new System.Windows.Forms.TextBox();
+            this.btnSeleccionaVideo = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 2;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Controls.Add(this.btnProcesaVideo, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.btnExtraer, 0, 0);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(224, 73);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 1;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(216, 34);
+            this.tableLayoutPanel2.TabIndex = 11;
+            // 
+            // btnProcesaVideo
+            // 
+            this.btnProcesaVideo.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnProcesaVideo.Location = new System.Drawing.Point(111, 3);
+            this.btnProcesaVideo.Name = "btnProcesaVideo";
+            this.btnProcesaVideo.Size = new System.Drawing.Size(102, 28);
+            this.btnProcesaVideo.TabIndex = 3;
+            this.btnProcesaVideo.Text = "Generar VIDEO";
+            this.btnProcesaVideo.UseVisualStyleBackColor = true;
+            this.btnProcesaVideo.Click += new System.EventHandler(this.btnProcesaVideo_Click);
+            // 
+            // btnExtraer
+            // 
+            this.btnExtraer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnExtraer.Location = new System.Drawing.Point(3, 3);
+            this.btnExtraer.Name = "btnExtraer";
+            this.btnExtraer.Size = new System.Drawing.Size(102, 28);
+            this.btnExtraer.TabIndex = 8;
+            this.btnExtraer.Text = "Extraer FRAMES";
+            this.btnExtraer.UseVisualStyleBackColor = true;
+            this.btnExtraer.Click += new System.EventHandler(this.btnExtraer_Click);
+            // 
+            // comboBoxVISION
+            // 
+            this.comboBoxVISION.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.comboBoxVISION.FormattingEnabled = true;
+            this.comboBoxVISION.Location = new System.Drawing.Point(224, 33);
+            this.comboBoxVISION.Name = "comboBoxVISION";
+            this.comboBoxVISION.Size = new System.Drawing.Size(216, 21);
+            this.comboBoxVISION.TabIndex = 10;
+            this.comboBoxVISION.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // checkBuenos
+            // 
+            this.checkBuenos.AutoSize = true;
+            this.checkBuenos.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.checkBuenos.Location = new System.Drawing.Point(446, 33);
+            this.checkBuenos.Name = "checkBuenos";
+            this.checkBuenos.Size = new System.Drawing.Size(217, 34);
+            this.checkBuenos.TabIndex = 9;
+            this.checkBuenos.Text = "Extraer buenos";
+            this.checkBuenos.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.label1.Location = new System.Drawing.Point(120, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 30);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Directorio/Fichero :";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // tbVideo
+            // 
+            this.tbVideo.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tbVideo.Location = new System.Drawing.Point(224, 3);
+            this.tbVideo.Name = "tbVideo";
+            this.tbVideo.Size = new System.Drawing.Size(216, 20);
+            this.tbVideo.TabIndex = 1;
+            // 
+            // btnSeleccionaVideo
+            // 
+            this.btnSeleccionaVideo.Dock = System.Windows.Forms.DockStyle.Left;
+            this.btnSeleccionaVideo.Location = new System.Drawing.Point(446, 3);
+            this.btnSeleccionaVideo.Name = "btnSeleccionaVideo";
+            this.btnSeleccionaVideo.Size = new System.Drawing.Size(40, 24);
+            this.btnSeleccionaVideo.TabIndex = 2;
+            this.btnSeleccionaVideo.Text = "...";
+            this.btnSeleccionaVideo.UseVisualStyleBackColor = true;
+            this.btnSeleccionaVideo.Click += new System.EventHandler(this.btnSeleccionaVideo_Click);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33332F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33334F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33334F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Controls.Add(this.btnSeleccionaVideo, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.tbVideo, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.checkBuenos, 2, 1);
+            this.tableLayoutPanel1.Controls.Add(this.comboBoxVISION, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 1, 2);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 4;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(666, 135);
+            this.tableLayoutPanel1.TabIndex = 10;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(666, 135);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "UTILIDADES DE VIDEO";
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.Button btnProcesaVideo;
+        private System.Windows.Forms.Button btnExtraer;
+        private System.Windows.Forms.ComboBox comboBoxVISION;
+        private System.Windows.Forms.CheckBox checkBuenos;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbVideo;
+        private System.Windows.Forms.Button btnSeleccionaVideo;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,297 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using Emgu.CV;
+using Emgu.CV.Structure;
+
+namespace BlackFrameEraser
+{
+    public partial class MainForm : Form
+    {
+        // Content item for the combo box
+        private class Item
+        {
+            public string Name;
+            public int Value;
+            public Item(string name, int value)
+            {
+                Name = name; Value = value;
+            }
+            public override string ToString()
+            {
+                // Generates the text shown in the combo box
+                return Name;
+            }
+        }
+
+        // string pathVideo = "";
+        string[] files = { };
+        int indiceImagen = -1;
+        int contadorFrames = 0;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            comboBoxVISION.Items.Add(new Item("PANASONIC", 1));
+            comboBoxVISION.Items.Add(new Item("KEYENCE", 2));
+        }
+
+        private void btnSeleccionaVideo_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.Filter = "MPG (*.mpg)|*.mpg|Avi (*.avi)|*.avi|All Files (*.*)|*.*";
+            dlg.Multiselect = true;
+            if (dlg.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                files = dlg.FileNames;
+                if (files.Length > 0)
+                    tbVideo.Text = Path.GetDirectoryName(files[0]);
+                else
+                    tbVideo.Text = "No files";
+            }
+        }
+
+        private void btnProcesaVideo_Click(object sender, EventArgs e)
+        {
+            contadorFrames = 0; 
+            if ((Item)comboBoxVISION.SelectedItem != null && ((Item)comboBoxVISION.SelectedItem).Value > 0)
+            {
+                if (((Item)comboBoxVISION.SelectedItem).Value == 1) checkBuenos.Checked = true; //PANASONIC
+                if (files.Length > 0)
+                {
+                    foreach (string pathVideo in files)
+                    {
+                        if (File.Exists(pathVideo))
+                        {
+                            LimpiaFrames(pathVideo);
+                        }
+                        else
+                        {
+                            MessageBox.Show("No existe el fichero \r\n" + pathVideo);
+                        }
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("No hay ficheros en: \r\n" + tbVideo.Text);
+                }
+            }
+            else { MessageBox.Show("Seleccione sistema de vision"); }
+        }
+
+        private void LimpiaFrames(string _path)
+        {
+            Emgu.CV.Capture videoStream = new Emgu.CV.Capture(_path);
+            int videoW = videoStream.Width;
+            int videoH = videoStream.Height;
+            int videoFps = 30;
+
+            string saveDir = Path.GetDirectoryName(_path);
+            string extension = Path.GetExtension(_path);
+            string baseName = Path.GetFileName(_path).Replace(extension, "");
+
+            string saveFileName = Path.Combine(saveDir, baseName + "_" + comboBoxVISION.Text + ".avi");
+
+            bool bSigueProcesando = true;
+
+            VideoWriter vw = new VideoWriter(saveFileName, -1, videoFps, new Size(videoW, videoH), true);
+            // VideoWriter vw = new VideoWriter(saveFileName, VideoWriter.Fourcc('D', 'I', 'V', 'X'), videoFps, new Size(videoW, videoH), true);
+
+            System.IO.Directory.CreateDirectory(saveDir);
+
+            indiceImagen = -1;
+            while (bSigueProcesando)
+            {
+                try
+                {
+                    using (Mat frame = videoStream.QueryFrame())
+                    {
+                        if (frame == null)
+                        {
+                            bSigueProcesando = false;
+                            continue;
+                        }
+
+                        //PANASONIC
+                        using ( Image<Bgr, byte> imageFrame = frame.Clone().ToImage<Bgr, byte>() )
+                        {
+                            // SI FRAME ES NEGRO => IGNORAR
+                            if (imageFrame.Split()[0].GetSum().Intensity < frame.Height * frame.Height * 50)
+                            {
+                                continue;
+                            }
+                            contadorFrames++;
+                            // frame.Save(Path.Combine(saveDir, "FRAME_" + baseName + "_" + (++contador) + ".jpg"));
+                            vw.Write(frame);
+                        }; //The image will be disposed here and memory freed
+                    }
+                }
+                catch (Exception ex)
+                {
+                    GC.WaitForPendingFinalizers();
+                }
+            }
+
+            MessageBox.Show("FIN - Video Procesado en: \r\n" + saveFileName);
+            vw.Dispose();
+            videoStream.Dispose();
+        }
+
+        private void ExtraeErrores(string _path)
+        {
+            Mat frameAnterior = null;
+
+            Emgu.CV.Capture videoStream = new Emgu.CV.Capture(_path);
+
+            string extension = Path.GetExtension(_path);
+            string baseName = Path.GetFileName(_path).Replace(extension, "");
+
+            string saveDir = Path.GetDirectoryName(_path);
+            System.IO.Directory.CreateDirectory(saveDir);
+
+            bool bSigueProcesando = true;
+
+            indiceImagen = -1;
+            int contador = -1;
+            double rojo = 0;
+            int frameDelay = 2;
+            bool salvaFrame = false;
+
+            while (bSigueProcesando)
+            {
+                try
+                {
+                    using (Mat frame = videoStream.QueryFrame())
+                    {
+                        if (frame == null)
+                        {
+                            bSigueProcesando = false;
+                            continue;
+                        }
+                        if (contador < 0)
+                        {
+                            frameAnterior = frame.Clone();
+                            contador = 0;
+                        }
+
+                        if (((Item)comboBoxVISION.SelectedItem).Value == 2)
+                        {
+                            // KEYENCE
+                            Rectangle roi = new Rectangle(frame.Cols / 4, 2 * (frame.Rows / 6), frame.Cols / 4, frame.Rows / 6);
+                            Mat roi_VISOK = new Mat(frame, roi);
+                            bool isRed = false;
+
+                            using (Image<Bgr, byte> frameOriginal = roi_VISOK.ToImage<Bgr, byte>())
+                            {
+                                rojo = frameOriginal.GetSum().Red;
+                                if (rojo > (frameOriginal.Width * frameOriginal.Height * 70))
+                                    isRed = true;
+                            }
+
+                            using (Image<Bgr, byte> imageDiff = frame.ToImage<Bgr, byte>().AbsDiff(frameAnterior.ToImage<Bgr, byte>()))
+                            {
+                                MCvScalar diff = imageDiff.GetSum().MCvScalar;
+                                if (isRed && diff.V0 > 3000000)
+                                {
+                                    contadorFrames++;
+                                    frame.Save(Path.Combine(saveDir, "ERR_" + baseName + "_" + (++contador) + ".jpg"));
+                                    frameAnterior = frame.Clone();
+                                }
+                                else if (diff.V0 > 3000000 && checkBuenos.Checked)
+                                {
+                                    frame.Save(Path.Combine(saveDir, "OK_" + baseName + "_" + (++contador) + ".jpg"));
+                                    frameAnterior = frame.Clone();
+                                }
+                            }; //The image will be disposed here and memory freed                
+                            // vw.Write(frame);
+                        }
+                        else if (((Item)comboBoxVISION.SelectedItem).Value == 1)
+                        {
+                            //PANASONIC
+                            // Rectangle roi = new Rectangle(480, 145 ,(635-480), (170-145));
+                            using (Image<Bgr, byte> imageDiff = frame.ToImage<Bgr, byte>().AbsDiff(frameAnterior.ToImage<Bgr, byte>()))
+                            {
+                                // SI FRAME ES NEGRO => IGNORAR
+                                if (frame.ToImage<Bgr, byte>().Split()[0].GetSum().Intensity < frame.Height * frame.Height * 50)
+                                {
+                                    contadorFrames++;
+                                    continue;
+                                }
+
+                                // SI FRAME ES SIMILAR AL ANTERIOR => IGNORAR
+                                MCvScalar diff = imageDiff.GetSum().MCvScalar;
+                                if (diff.V0 > 3000000)
+                                {
+                                    contadorFrames++;
+                                    salvaFrame = true;
+                                    // frame.Save(Path.Combine(saveDir, "FRAME_" + baseName + "_" + (++contador) + ".jpg"));
+                                    frameAnterior = frame.Clone();
+                                }
+                            }; //The image will be disposed here and memory freed
+
+                            if (salvaFrame)
+                            {
+                                frameDelay--;
+                                if (frameDelay == 0)
+                                {
+                                    frame.Save(Path.Combine(saveDir, "FRAME_" + baseName + "_" + (++contador) + ".jpg"));
+                                    salvaFrame = false;
+                                    frameDelay = 2;
+                                }
+                            }
+                            // vw.Write(frame);
+                          }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    GC.WaitForPendingFinalizers();
+                }
+            }
+        }
+
+        private void btnExtraer_Click(object sender, EventArgs e)
+        {
+            contadorFrames=0; 
+            if ((Item)comboBoxVISION.SelectedItem != null && ((Item)comboBoxVISION.SelectedItem).Value > 0)
+            {
+                if (((Item)comboBoxVISION.SelectedItem).Value == 1) checkBuenos.Checked = true; //PANASONIC
+
+                if (files.Length > 0)
+                {
+                    foreach (string pathVideo in files)
+                    {
+                        if (File.Exists(pathVideo))
+                        {
+                            ExtraeErrores(pathVideo);
+                        }
+                        else
+                        {
+                            MessageBox.Show("No existe el fichero \r\n" + pathVideo);
+                        }
+                    }
+                    MessageBox.Show("FINALIZADO\r\n" + contadorFrames + " ERRORES");
+                }
+                else
+                {
+                    MessageBox.Show("No hay ficheros en: \r\n" + tbVideo.Text);
+                }
+            }
+            else { MessageBox.Show("Seleccione sistema de vision"); }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (((Item)comboBoxVISION.SelectedItem).Value == 1) //PANASONIC
+            {
+                checkBuenos.Checked = true;
+            }
+        }
+    }
+}
